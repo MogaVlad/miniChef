e22d1f418e86fcd6669a4468ab854f62619960e4
MogaVlad <vladmmoga@gmail.com>
2026-05-08 19:51:27 +0300

    Updated main page

--- src/components/CommunityElement.tsx
+++ src/components/CommunityElement.tsx
@@ -4,8 +4,7 @@ import Image from 'next/image'
 export default function Recipe({src,title,text}:any) {
   return (
     <div className='flex flex-col items-center gap-5'>
-        <Image src={src} alt="community_element" className='mb-3 w-[300px] h-[300px]'></Image>
-        <h3 className='text-main'> {title} </h3>
+    <Image src={src} alt="community_element" className='mb-3 w-[160px] h-[160px]'></Image>        <h3 className='text-main'> {title} </h3>
         <p className="text-center w-[300px]"> {text} </p>
     </div>
   )
--- src/components/EnterTheCommunity.tsx
+++ src/components/EnterTheCommunity.tsx
@@ -10,7 +10,7 @@ export default function EnterTheCommunity() {
     <section className='items-center flex flex-col bg-gray-300 justify-center w-full h-[calc(100vh-92.25px)]'>
       <section className='container mx-auto p-4 items-center justify-center'> 
         <h2 className='text-center text-2xl mb-6 font-semibold'> Enter the community </h2>
-        <div className='flex gap-8 justify-center mt-[50px]'>
+        <div className='flex gap-10 justify-center mt-[50px]'>            
             <CommunityElement src = {community_1} title = "Find recipes" text = "Explore the recipes selected by our staff uploaded by our community"/>
             <CommunityElement src = {community_2} title = "Review recipes" text = "Evaluate and comment on the dishes proposed by others"/>
             <CommunityElement src = {community_3} title = "Add recipes" text = "Pass on your know-how by proposing your recipes"/>
